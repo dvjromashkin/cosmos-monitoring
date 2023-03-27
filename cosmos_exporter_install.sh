79206af13571dea5dc77f9555c534e41912d9542
mogoll92 <mogoll92@gmail.com>
2023-03-27 19:40:30 +0300

    Separate exporters

--- cosmos_exporter_install.sh
+++ cosmos_exporter_install.sh
@@ -3,7 +3,7 @@
 source <(curl -s https://raw.githubusercontent.com/R1M-NODES/utils/master/common.sh)
 
 read -p "Enter bond_denom value, for example, [ubld for Agoric]: " BOND_DENOM
-read -p "Enter bench_prefix value, for example, [agoric for Agoric]: " BENCH_PREFIX
+read -p "Enter bench_prefix value, for example, [agoric for Agoric]: " BECH_PREFIX
 read -p "Enter rpc_port value or hit Enter for default port [26657]: " RPC_PORT
 RPC_PORT=${RPC_PORT:-26657}
 read -p "Enter port value for cosmos exporter or hit Enter for default port [9300]: " COSMOS_EXPORTER_PORT
@@ -13,7 +13,7 @@ GRPC_PORT=${GRPC_PORT:-9090}
 
 printDilimeter
 printGreen "Bond denom: \e[1m\e[32m$BOND_DENOM\e[0m"
-printGreen "Bench prefix: \e[1m\e[32m$BENCH_PREFIX\e[0m"
+printGreen "Bench prefix: \e[1m\e[32m$BECH_PREFIX\e[0m"
 printGreen "RPC port: \e[1m\e[32m$RPC_PORT\e[0m"
 printGreen "gRPC port: \e[1m\e[32m$GRPC_PORT\e[0m"
 printGreen "Cosmos exporter port: \e[1m\e[32m$COSMOS_EXPORTER_PORT\e[0m"
@@ -35,11 +35,11 @@ else
 fi
 
 printDelimiter
-printGreen "Setup service: cosmos-${BENCH_PREFIX}-exporter.service"
+printGreen "Setup service: cosmos-${BECH_PREFIX}-exporter.service"
 
-sudo tee <<EOF >/dev/null /etc/systemd/system/cosmos-${BENCH_PREFIX}-exporter.service
+sudo tee <<EOF >/dev/null /etc/systemd/system/cosmos-${BECH_PREFIX}-exporter.service
 [Unit]
-Description=Cosmos ${BENCH_PREFIX^} Exporter
+Description=Cosmos ${BECH_PREFIX^} Exporter
 After=network-online.target
 
 [Service]
@@ -48,7 +48,7 @@ Group=cosmos_exporter
 TimeoutStartSec=0
 CPUWeight=95
 IOWeight=95
-ExecStart=cosmos-exporter --denom ${BOND_DENOM} --denom-coefficient 1000000 --bech-prefix ${BENCH_PREFIX} --tendermint-rpc http://localhost:${RPC_PORT} --node localhost:${GRPC_PORT} --listen-address :${COSMOS_EXPORTER_PORT}
+ExecStart=cosmos-exporter --denom ${BOND_DENOM} --denom-coefficient 1000000 --bech-prefix ${BECH_PREFIX} --tendermint-rpc http://localhost:${RPC_PORT} --node localhost:${GRPC_PORT} --listen-address :${COSMOS_EXPORTER_PORT}
 Restart=always
 RestartSec=2
 LimitNOFILE=800000
@@ -59,8 +59,8 @@ WantedBy=multi-user.target
 EOF
 
 sudo systemctl daemon-reload
-sudo systemctl enable cosmos-${BENCH_PREFIX}-exporter
-sudo systemctl start cosmos-${BENCH_PREFIX}-exporter
+sudo systemctl enable cosmos-${BECH_PREFIX}-exporter
+sudo systemctl start cosmos-${BECH_PREFIX}-exporter
 
 printGreen "Installation finished" && sleep 1
 printGreen "Please make sure ports $COSMOS_EXPORTER_PORT is open" && sleep 1
