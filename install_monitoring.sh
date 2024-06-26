d0148c13179f5500d0250e794c2fbc6786e96872
Roman Shaulskyi <56173234+dvjromashkin@users.noreply.github.com>
2024-06-26 16:32:34 +0300

    Update install_monitoring.sh

--- install_monitoring.sh
+++ install_monitoring.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source <(curl -s https://raw.githubusercontent.com/staketown/utils/master/common.sh)
-
 printLogo
 
 echo ""
@@ -16,13 +14,25 @@ sudo pip install yq
 
 echo ""
 printGreen "Install Docker and docker compose" && sleep 1
-bash <(curl -s https://raw.githubusercontent.com/staketown/utils/master/docker-install.sh)
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg lsb-release
 
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo usermod -aG docker $USER
 
 echo ""
 printGreen "Downloading Node Monitoring config files" && sleep 1
 cd $HOME
 rm -rf cosmos_monitoring
-git clone https://github.com/staketown/cosmos_monitoring.git
+git clone https://github.com/dvjromashkin/cosmos_monitoring.git
 
 chmod +x $HOME/cosmos_monitoring/add_validator.sh
